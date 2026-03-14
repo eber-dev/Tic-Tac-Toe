b2d19ce7b8fe28e5aa55679d2ceeb62de1b181a0
Eber <www.eberacunaquispe@gmail.com>
2026-03-13 20:17:16 -0500

    Se avanzo la logica del Gamecontroller la funcion de iniciar de camprobar ganador y la de iniciar en juego

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function player(name){
+function player(name,marca){
     return{
-        name
+        name,marca
     }
 }
 
@@ -11,8 +11,8 @@ const Gameboard = (function (){
         ["","",""]
     ]
     return{
-        colocarMarca(num1,num2){
-            board[num1][num2]
+        colocarMarca(num1,num2,marca){
+            board[num1][num2] = marca
         },
         obtenerTablero(){
             let linea1 = ""
@@ -37,6 +37,11 @@ const Gameboard = (function (){
             console.log(separador)
             console.log(linea3)     
         },
+        verificarCasilla(fila,columna){
+            if(board[fila][columna] != ""){
+                return
+            }
+        },
         reiniciar(){
             for(let i = 0; i<board.length; i++){
                 for(let j=0; j<board[i].length;j++){
@@ -49,15 +54,93 @@ const Gameboard = (function (){
 })()
 
 const Gamecontroller =(function (){
+    let player1, player2,actual
     return{
-        iniciarJuego(){
+        playervsplayer(name1,name2){
+            player1 = player(name1,"X")
+            player2 = player(name2,"O")
+            return "modo1"
+        },
+        playervspc(name1){
+            player1 = player(name1,"X")
+            player2 = player("PC","O")
+            return "modo2"
+        },
+        iniciarJuego(modo){
+            switch(modo){
+                case "modo1":
+                    actual = player1
+                    break;
+                case "modo2":
+                    actual = player1
+                    break;
+            }
 
         },
         cambiarTurnos(){
-
+            if(actual == player1){
+                actual = player2
+                return
+            }else if(actual == player2){
+                actual =player1
+                return
+            }
         },
-        verificarGanador(){
+        verificarGanador(matriz,modo){
+            let validador = ""
+            let validador2 = ""
+            let validador3 = ""
+            
+            for(let i=0; i<matriz;i++){
+                validador+= matriz[i][i]
+                if(validador == "XXX"){
+                    console.log(`Ganador ${player1}`)
+                    return true
+                }else if( validador == "OOO"){
+                    if(modo == "modo1"){
+                        console.log(`Ganador ${player2}`)
+                        return true
+                    }else if(modo == "modo2"){
+                        console.log(`Ganador PC`)
+                        return true
+                    }
+                }
+            }
 
+            for(let i=0; i<matriz;i++){
+                validador+= matriz[i][matriz.length-1-i]
+                if(validador == "XXX"){
+                    console.log(`Ganador ${player1}`)
+                    return true
+                }else if( validador == "OOO"){
+                    if(modo == "modo1"){
+                        console.log(`Ganador ${player2}`)
+                        return true
+                    }else if(modo == "modo2"){
+                        console.log(`Ganador PC`)
+                        return true
+                    }
+                }
+            }
+
+            for(let i=0; i<matriz;i++){
+                validador+= matriz[0][i]
+                validador2+= matriz[1][i]
+                validador3+= matriz[2][i]
+
+                if(validador == "XXX" || validador2 == "XXX" || validador3 == "XXX" ){
+                    console.log(`Ganador ${player1}`)
+                    return true
+                }else if( validador == "OOO" || validador2 == "OOO" || validador3 == "OOO"){
+                    if(modo == "modo1"){
+                        console.log(`Ganador ${player2}`)
+                        return true
+                    }else if(modo == "modo2"){
+                        console.log(`Ganador PC`)
+                        return true
+                    }
+                }
+            }
         },
         verificarEmpate(){
 
